7e3507e927a4b51944f577ac23adcafd1af47e95
kapufskipauw <rosarhode@gmail.com>
2025-12-09 16:39:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First PHP Calve</title>
+</head>
+<body>
+
+    <h1>
+        <?php 
+            echo "Hello World!";
+        ?>
+    </h1>
+    
+</body>
+</html>

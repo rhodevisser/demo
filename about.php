4a28a630b0becf6d2f379c86716b5dfdead8176d
kapufskipauw <rosarhode@gmail.com>
2025-12-23 20:14:38 +0100

    feat: Added variable
    And placed it in the header to let each paged has it's own header

--- about.php
+++ about.php
@@ -1,3 +1,5 @@
 <?php
 
+$heading = "About Moi";
+
 require "views/about.view.php";
--- index.php
+++ index.php
@@ -1,36 +1,6 @@
-  <?php 
+<?php 
 
-        $title = "My Favourite Tv-Shows";
+$heading = 'Home';
 
-        $tvshows = [
-            [
-            'name'=> 'Gossip Girl',
-            'onAir' => '2007 - 2012',
-            'writer'=> 'Josh Schwartz',
-            'wikipediaLink' => 'https://en.wikipedia.org/wiki/Gossip_Girl'
-            ], 
-            [
-            'name'=> 'The OC',
-            'onAir' => '2003 - 2007',
-            'writer' => 'Josh Schwartz',
-            'wikipediaLink' => 'https://en.wikipedia.org/wiki/The_O.C.' 
-            ],
-            [
-            'name'=> 'Slicon Valley',
-            'onAir' => '2014 - 2019',
-            'writer' => 'Mike Judge',
-            'wikipediaLink' => 'https://en.wikipedia.org/wiki/Silicon_Valley_(TV_series)'
-            ],
-            [
-            'name' => 'True Blood',
-            'onAir' => '2008 - 2014',
-            'writer' => 'Alan Ball',
-            'wikipediaLink' => 'https://en.wikipedia.org/wiki/True_Blood'
-            ]
-        ];
-        
-        $filteredTvShows = array_filter($tvshows, function($tvshow) {
-            return $tvshow['onAir'] > 2007;
-    });
 
 require "views/index.view.php";

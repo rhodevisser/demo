0894c93bd8ce9cbbceb1d0794eaecf77bbd1441a
kapufskipauw <rosarhode@gmail.com>
2025-12-27 15:07:49 +0100

    end: Removed the old code
    Because I put it into the nav file

--- index.php
+++ index.php
@@ -11,13 +11,7 @@ die();
 
 }
 
-if ($_SERVER['REQUEST_URI'] === '/laracast-websites/demo/index.php' ) { 
-    echo 'bg-black 900 text-white'; 
-} else {
-    echo 'text-gray-300';
-}
 
-echo $_SERVER['REQUEST_URI'] === '/laracast-websites/demo/index.php' ? 'bg-black 900 text-white' : 'text-gray-300'; 
 
 
 

5b7a567e81f769be2165c951899a7f93a78a72a3
kapufskipauw <rosarhode@gmail.com>
2025-12-29 15:16:43 +0100

    fix: Now fn can acces values outside scope
    Added key and value to fn so it can acces

--- index.php
+++ index.php
@@ -18,10 +18,12 @@ function abort($code = 404) {
     die();
 }
 
-function routeToController() {
+function routeToController($uri, $routes) {
     if (array_key_exists($uri, $routes)) {
         require $routes[$uri];
     } else {
     abort(); 
     }
 }
+
+routeToController($uri, $routes);

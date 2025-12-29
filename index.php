0c63206f5192c674f4653459078b6f11a095068d
kapufskipauw <rosarhode@gmail.com>
2025-12-29 12:29:30 +0100

    feat: Added URI inspector
    By using parse_url() function and setting it to the key 'path' it isolates the path

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 require 'functions.php';
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI']) ['path'] ;
 
 if ($uri === '/') {
     require 'controllers/index.php';
 } else if ($uri === '/about') {
     require 'controllers/about.php';
-} else {
-    require 'controllers/about.php';
+} else if ($uri === '/contact') {
+    require 'controllers/contact.php';
 }
 

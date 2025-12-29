2c05c87451b7fc601aad0b44ef8b5f59b918f247
kapufskipauw <rosarhode@gmail.com>
2025-12-29 15:10:05 +0100

    feat: Created 404 page
    Added 404 file and function that gives 404 response if the url input is not known to the website

--- index.php
+++ index.php
@@ -10,6 +10,16 @@ $routes = [
     '/contact' =>  'controllers/contact.php',
 ];
 
+function abort($code = 404) {
+    http_response_code($code);
+
+    require "views/{$code}.php"; 
+
+    die();
+}
+
 if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
+} else {
+   abort(); 
 }

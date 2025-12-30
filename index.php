62553516a206a18369c2ac2482b31e4e7e9dfea9
kapufskipauw <rosarhode@gmail.com>
2025-12-30 11:04:29 +0100

    end: Removed exercise parts

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php 
 
 require 'functions.php';
-// require 'router.php';
+require 'router.php';
 
 // connect to mySQL database. 
 
-class Person {
-    public $name;
-    public $age;
-
-    public function breathe() {
-        echo $this->name . ' is breathing!';
-    }
-}
-
-$person = new Person();
-
-$person->name = 'John Doe'; 
-$person->age = 25;
-
-$person->breathe();

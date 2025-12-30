6c0e7e1762e17ee8f3fe445797ce48ce17a510d7
kapufskipauw <rosarhode@gmail.com>
2025-12-30 15:55:41 +0100

    add: mySQL DB
    Added connection string and created  class to connect with mySQL database

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
 
 // connect to mySQL database. 
+$dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
+$pdo = new PDO($dsn);
+
 

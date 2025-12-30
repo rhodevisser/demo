f7772f124811ba071e3b5a535caef37c0b10cdb9
kapufskipauw <rosarhode@gmail.com>
2025-12-30 10:57:26 +0100

    exercise: Intro to PHP class

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php 
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to mySQL database. 
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe() {
+        echo "breathing";
+    }
+}
+
+$person = new Person();
+
+$person->name = 'John Doe'; 
+$person->age = 25;
+
+dumbDie($person->age);

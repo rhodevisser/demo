6b4e502ad31c781f2346d508a30a33e8b4d4733b
kapufskipauw <rosarhode@gmail.com>
2025-12-30 15:58:08 +0100

    add: Added mySQL query
    Prepared a query to send to mySQL and mySQL executed that query

--- index.php
+++ index.php
@@ -7,4 +7,7 @@ require 'functions.php';
 $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
 $pdo = new PDO($dsn);
 
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
 

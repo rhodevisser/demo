92e9901cdf2ed8cea5dff650db7b31218218a493
kapufskipauw <rosarhode@gmail.com>
2025-12-31 12:34:41 +0100

    chore: Removed unnecessary text

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ require 'functions.php';
 require 'Database.php';
 //require 'router.php';
 
-// Connect to the database, and execute a query. 
-
-
 $db = new Database();
 
 $post = $db->query("select * from posts where id = 2")->fetch(PDO::FETCH_ASSOC);

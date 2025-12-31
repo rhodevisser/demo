31af635e39d09157c4a33bcd663e9c002064f433
kapufskipauw <rosarhode@gmail.com>
2025-12-31 11:49:25 +0100

    refactor: Editted old code into fn

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ require 'functions.php';
 //require 'router.php';
 
 // Connect to the database, and execute a query. 
-class_Database {
+class Database {
     public function query()
     {
+        $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
+        $pdo = new PDO($dsn);
+
+        $statement = $pdo->prepare("select * from posts");
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
 
     }
 
@@ -14,18 +21,12 @@ class_Database {
 
 $db = new Database();
 
-$db->query();
+$posts = $db->query();
 
 
 
 // connect to mySQL database. 
-$dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
-$pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
         echo "<li>" .$post['title'] ."</li>";

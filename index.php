9db034c36527bad8577301d31c03f1a034b4b214
kapufskipauw <rosarhode@gmail.com>
2025-12-31 11:42:38 +0100

    refactor: Created class for database
    This is what I am going to use to refactor the // connect to mySQL database part in so the code will be more manageble

--- index.php
+++ index.php
@@ -3,6 +3,21 @@
 require 'functions.php';
 //require 'router.php';
 
+// Connect to the database, and execute a query. 
+class_Database {
+    public function query()
+    {
+
+    }
+
+}
+
+$db = new Database();
+
+$db->query();
+
+
+
 // connect to mySQL database. 
 $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
 $pdo = new PDO($dsn);

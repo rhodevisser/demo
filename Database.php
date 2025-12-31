e290c364c40649598750891d100e2e83b5b866bb
kapufskipauw <rosarhode@gmail.com>
2025-12-31 12:33:06 +0100

    refactor: Created Database file
    It is with a capital, because it only contains a class

---
+++ Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+
+    public $connection;
+
+    public function __construct()
+    {
+
+        $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
+
+        $this->connection = new PDO($dsn);
+
+    }
+
+
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+
+        return $statement;
+
+    }
+
+}
--- index.php
+++ index.php
@@ -1,33 +1,11 @@
 <?php 
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
 // Connect to the database, and execute a query. 
-class Database {
 
-    public $connection;
-
-    public function __construct()
-    {
-
-        $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
-
-        $this->connection = new PDO($dsn);
-
-    }
-
-
-    public function query($query)
-    {
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement;
-
-    }
-
-}
 
 $db = new Database();
 

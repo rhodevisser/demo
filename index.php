e74b493607711707bed918eb8f2228025eb30760
kapufskipauw <rosarhode@gmail.com>
2025-12-31 12:01:17 +0100

    refactor: Use constructor to initialize PDO
    Used the constructor method to initialize PDO connection once when the class instance is created, so it will not repeat all of it.

--- index.php
+++ index.php
@@ -5,11 +5,15 @@ require 'functions.php';
 
 // Connect to the database, and execute a query. 
 class Database {
-    public function query($query)
+    public function __construct()
     {
         $dsn = "mysql:host=localhost;port=3306;user=root;dbname=mydemoapp;utf8mb4";
         $pdo = new PDO($dsn);
+    }
 
+
+    public function query($query)
+    {
         $statement = $pdo->prepare($query);
         $statement->execute();
 

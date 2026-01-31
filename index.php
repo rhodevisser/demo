e59da1c6f8335cffdcc6ed186c68c9d1323d09c5
kapufskipauw <rosarhode@gmail.com>
2026-01-31 21:21:25 +0100

    #1 fix:
    The problem was that code was GETting whatever the user writes for id parameter which get in-lined in the query send to DB.
    
    Instead use prepared statements and bound parameters. This so SQL goes to db in one request, and then the parameters (the user data) go it's own request.

--- index.php
+++ index.php
@@ -4,15 +4,21 @@ require 'functions.php';
 require 'Database.php';
 //require 'router.php';
 
+
 $config = require('config.php');
 
 $db = new Database($config['database']);
 
+
 $id = $_GET['id'];
+$query = "select * from posts where id = ? ";
 
-$post = $db->query("select * from posts where id = {$id}")->fetch();
 
-dumbDie($post);
+
+$post = $db->query($query, [$id])->fetch();
+
+
+// dumbDie($id, $post);
 
 
 
